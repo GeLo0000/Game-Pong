f784fbf614e8e5166a5305fca32c36df4ca368d8
Oleg <baranov.off2@gmail.com>
2026-01-19 16:35:59 +0200

    Add .clang-format for C++

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,15 +1,11 @@
 #include "Game.hpp"
 
-Game::Game()
-    : m_window(sf::VideoMode({800u, 600u}), "Pong Game")
-{
+Game::Game() : m_window(sf::VideoMode({800u, 600u}), "Pong Game") {
     m_window.setFramerateLimit(144);
 }
 
-void Game::run()
-{
-    while (m_window.isOpen())
-    {
+void Game::run() {
+    while (m_window.isOpen()) {
         float dt = m_clock.restart().asSeconds();
         processEvents();
         update(dt);
@@ -17,37 +13,26 @@ void Game::run()
     }
 }
 
-void Game::processEvents()
-{
-    while (const std::optional ev = m_window.pollEvent())
-    {
-        if (ev->is<sf::Event::Closed>())
-        {
+void Game::processEvents() {
+    while (const std::optional ev = m_window.pollEvent()) {
+        if (ev->is<sf::Event::Closed>()) {
             m_window.close();
-        }
-        else if (const auto *kp = ev->getIf<sf::Event::KeyPressed>())
-        {
-            if (kp->scancode == sf::Keyboard::Scancode::Escape)
-            {
+        } else if (const auto *kp = ev->getIf<sf::Event::KeyPressed>()) {
+            if (kp->scancode == sf::Keyboard::Scancode::Escape) {
                 m_window.close();
             }
         }
     }
 
     // Also allow holding ESC to close quickly
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Escape))
-    {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Escape)) {
         m_window.close();
     }
 }
 
-void Game::update(float deltaTime)
-{
+void Game::update(float deltaTime) {}
 
-}
-
-void Game::render()
-{
+void Game::render() {
     m_window.clear(sf::Color::White);
     m_window.display();
 }
